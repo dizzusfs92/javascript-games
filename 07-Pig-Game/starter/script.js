92991f0ee7035a3c2a1430bca0fc2f560eefc602
Diego Fernandes da Silva <dizzusmon@gmail.com>
2024-06-07 15:20:02 -0300

    ajustes no jogo pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -19,37 +19,62 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 let currentScore = 0;
-let currentPlayerEl = current0El;
+let currentPlayer = '0';
+let scores = [score0El, score1El];
+let playing = true;
 
-const changePlayer = (currentPlayer) => {
-    currentPlayer === current0El ? (player1.classList.add('player--active'), player0.classList.remove('player--active')) : (player0.classList.add('player--active'), player1.classList.remove('player--active'));
-    return (currentPlayer === current0El ? current1El : current0El);
+const switchPlayer = function(player){
+    document.querySelector('.player--'+player).classList.remove('player--active');
+    currentPlayer = (player === '0' ? '1' : '0');
+    document.querySelector('.player--'+currentPlayer).classList.add('player--active');
 }
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function(){
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    diceEl.src = `dice-${dice}.png`;
-    diceEl.classList.remove('hidden');
-
-    if (dice != 1) {
-        currentScore += dice;
-        currentPlayerEl.textContent = currentScore;
-    }
-    else{
-        currentPlayerEl.textContent = 0;
-        currentPlayerEl = changePlayer(currentPlayerEl);
-        currentScore = 0;
+    if (playing) {
+        const dice = Math.trunc(Math.random() * 6) + 1;
+        diceEl.src = `dice-${dice}.png`;
+        diceEl.classList.remove('hidden');
+    
+        if (dice != 1) {
+            currentScore += dice;
+            document.querySelector('#current--'+currentPlayer).textContent = currentScore;
+        }
+        else{
+            document.querySelector('#current--'+currentPlayer).textContent = 0;
+            switchPlayer(currentPlayer);
+            currentScore = 0;
+        }
     }
 });
 
 btnHold.addEventListener('click', function(){
-    console.log('dddas');
-    currentPlayerEl === current0El ? score0El.textContent = Number(score0El.textContent) + currentScore : score1El.textContent = Number(score1El.textContent) + currentScore;
-    currentPlayerEl = changePlayer(currentPlayerEl);
+    if (playing) {
+        document.querySelector('#current--'+currentPlayer).textContent = 0;
+        scores[currentPlayer].textContent = Number(scores[currentPlayer].textContent) + currentScore;
+        if (Number(scores[currentPlayer].textContent) >= 20) {
+            document.querySelector('.player--'+currentPlayer).classList.add('player--winner');
+            document.querySelector('#name--'+currentPlayer).classList.add('player--winner','name');
+            playing = false;
+        }
+        else {
+            switchPlayer(currentPlayer);
+        }
+        currentScore = 0;
+    }
 });
 
-
+btnNew.addEventListener('click', function(){
+    document.querySelector('.player--'+currentPlayer).classList.remove('player--winner');
+    document.querySelector('#name--'+currentPlayer).classList.remove('player--winner');
+    currentPlayer = '0';
+    document.querySelector('.player--0').classList.add('player--active');
+    document.querySelector('.player--1').classList.remove('player--active');
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    diceEl.classList.add('hidden');
+    playing = true;
+})
 
 
 
